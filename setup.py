a1937651ef0d5c1bb52f054d968c07bf27a87bf5
Tovin-thomas <tovinthomas2@gmail.com>
2025-12-20 01:16:34 +0530

    requirements are addedd and setup.py also addedd

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical-chartboat",
+    version="0.1.0",
+    author="Tovin",
+    author_email="tovin@example.com",
+    packages=find_packages(),
+    install_requires=[]
+)
